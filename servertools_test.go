259de289d079db9eb37771d281f7b58d24b53425
Phisto <simon.cay.gaus@gmail.com>
2023-11-15 13:30:21 +0100

    Add tests

---
+++ servertools_test.go
@@ -0,0 +1,17 @@
+package servertools_test
+
+import (
+	"testing"
+)
+
+func TestLoadServerCertificateHandler(t *testing.T) {
+
+	/*
+		handler := festivalspki.LoadServerCertificateHandler("certificates/*.festivalsapp.dev.crt", "certificates/*.festivalsapp.dev.key", "certificates/festivalsapp-development-root-ca.crt")
+		_, err := handler(&tls.ClientHelloInfo{})
+		if err != nil {
+			t.Errorf("Handler failed to load server certificates.")
+		}*/
+
+	t.Log("running TestLoadX509Certificate")
+}
